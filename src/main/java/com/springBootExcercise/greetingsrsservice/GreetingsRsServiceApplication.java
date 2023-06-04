546ee8d9954eb58169dfcdee7890018ca430eb4a
Alfredo Salamone <salamonealfredo@gmail.com>
2023-06-04 18:24:37 +0200

    Primo esercizio guida SpringBoot

---
+++ src/main/java/com/springBootExcercise/greetingsrsservice/GreetingsRsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.springBootExcercise.greetingsrsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingsRsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingsRsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springBootExcercise/greetingsrsservice/GreetingsRsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBootExcercise.greetingsrsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingsRsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
